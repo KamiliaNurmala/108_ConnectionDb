48f2392c74f51c94065f6a25597e82335843fd51
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-15 12:12:10 +0700

    add db.connect callback logging

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const db = mysql.createConnection({
   database: 'mahasiswa'
 });
 
+db.connect((err) => {
+  if (err) {
+    console.log('Error connecting to mysql:', err);
+    return;
+  }
+  console.log('Connected to mysql database');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
